f8ae529a235f9705833c00a5fcd8a78d3927ab02
Frago9876543210 <al.volf.av@gmail.com>
2020-03-02 20:45:39 +0300

    SpecterInfo: drop redundant clientId param

--- src/Frago9876543210/Specter/SpecterInfo.php
+++ src/Frago9876543210/Specter/SpecterInfo.php
@@ -11,14 +11,14 @@ use function str_repeat;
 
 class SpecterInfo extends PlayerInfo{
 
-	public function __construct(string $username, ?UUID $uuid = null, ?Skin $skin = null, ?string $locale = null, ?string $xuid = null, ?int $clientId = null){
+	public function __construct(string $username, ?UUID $uuid = null, ?Skin $skin = null, ?string $locale = null, ?string $xuid = null, ?array $extraData = []){
 		parent::__construct(
 			$username,
 			$uuid ?? UUID::fromRandom(),
 			$skin ?? new Skin("Standard_Custom", str_repeat("\x80", 8192)),
 			$locale ?? "en_US",
 			$xuid ?? "",
-			[]
+			$extraData ?? []
 		);
 	}
 }
